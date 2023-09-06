b7103eab8d7c252134a5294cd3943469f12f6aad
Gustavo Santos Teixeira <136706241+Gustavovo17102003@users.noreply.github.com>
2023-09-06 17:12:41 -0300

    Add files via upload

---
+++ fila_dinamica#1.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int posicao = 0;
+int tamanho;
+
+struct Lista
+{
+    int *lista;
+};
+struct Lista l;
+
+void ADD_Inicio(int valor)
+{
+    if (posicao == 0)
+    {
+        l.lista = (int *)malloc(sizeof(int));
+        l.lista[posicao] = valor;
+        posicao++;
+    }
+    else
+    {
+        int *nova_lista = (int *)malloc((posicao + 1) * sizeof(int));
+        nova_lista[0] = valor;
+
+        for (int i = 0; i < posicao; i++)
+        {
+            nova_lista[i + 1] = l.lista[i];
+        }
+
+        free(l.lista);
+        l.lista = nova_lista;
+        posicao++;
+    }
+}
+
+void Add_fim(int valor)
+{
+    int tamanho_atual = posicao;
+
+    if (tamanho_atual == 0)
+    {
+        ADD_Inicio(valor);
+    }
+    else
+    {
+        int *nova_lista = (int *)malloc((tamanho_atual + 1) * sizeof(int));
+
+        for (int i = 0; i < tamanho_atual; i++)
+        {
+            nova_lista[i] = l.lista[i];
+        }
+
+        nova_lista[tamanho_atual] = valor;
+
+        free(l.lista);
+        l.lista = nova_lista;
+        posicao++;
+    }
+}
+
+void Remove_Inicio()
+{
+    if (posicao == 0)
+    {
+        printf("Lista vazia, não é possível remover do início.\n");
+        return -1;
+    }
+
+    int valor_removido = l.lista[0];
+
+    int *nova_lista = (int *)malloc((posicao - 1) * sizeof(int));
+
+    for (int i = 1; i < posicao; i++)
+    {
+        nova_lista[i - 1] = l.lista[i];
+    }
+
+    free(l.lista);
+    l.lista = nova_lista;
+    posicao--;
+}
+
+void Remove_Fim()
+{
+    if (posicao == 0)
+    {
+        printf("Lista vazia, não é possível remover do fim.\n");
+        return -1; // Indicar que a lista está vazia
+    }
+
+    int valor_removido = l.lista[posicao - 1];
+
+    int *nova_lista = (int *)malloc((posicao - 1) * sizeof(int));
+
+    for (int i = 0; i < posicao - 1; i++)
+    {
+        nova_lista[i] = l.lista[i];
+    }
+
+    free(l.lista);
+    l.lista = nova_lista;
+    posicao--;
+}
+
+void Mostrar()
+{
+    printf("\nLista:\n");
+    int i = 0;
+    while (i < posicao)
+    {
+        printf("%d ", l.lista[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    ADD_Inicio(1);
+    ADD_Inicio(2);
+
+    Add_fim(3);
+    Add_fim(4);
+
+    Mostrar();
+
+    Remove_Fim();
+
+    Mostrar();
+
+    Remove_Inicio();
+
+    Mostrar();
+
+    return 0;
+}
